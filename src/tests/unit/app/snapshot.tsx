b75e2bc1b08f56f202bb43de1aa81e4840fddd66
sharpwit <khosravi.webmaster@gmail.com>
2025-10-20 05:44:45 +0330

    chore(test): add Jest and React Testing Library
    Installed, Configured and Added basic test setup
    
    add pre-push for test in husky

---
+++ src/tests/unit/app/snapshot.tsx
@@ -0,0 +1,7 @@
+import Home from '@/app/page'
+import { render } from '@testing-library/react'
+
+it('renders homepage unchanged', () => {
+  const { container } = render(<Home />)
+  expect(container).toMatchSnapshot()
+})

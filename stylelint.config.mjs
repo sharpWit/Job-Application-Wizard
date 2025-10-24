cc2d70ae8d6f2522f92adbe006b496e9b677d509
sharpwit <khosravi.webmaster@gmail.com>
2025-10-24 07:25:12 +0330

    chore(styles): add some rules to stylelint
    added scripts for linting stylelint scss

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('stylelint').Config} */
 export default {
   extends: ['stylelint-config-standard-scss'],
+  rules: {
+    'selector-class-pattern': '^[a-z][a-z0-9]*(-[a-z0-9]+)*$',
+    'selector-id-pattern': null,
+    'scss/dollar-variable-pattern': '^[a-z][a-z0-9]*(-[a-z0-9]+)*$',
+    'scss/percent-placeholder-pattern': '^[a-z][a-z0-9]*(-[a-z0-9]+)*$',
+  },
 }
